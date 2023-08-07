a59b6554b654e35c9dd0cc71b12eb6b3cf915569
Gürkan <gurkanyigitgr@gmail.com>
2023-08-07 22:38:51 +0300

    add data-testids

--- pages/index.js
+++ pages/index.js
@@ -28,36 +28,39 @@ export default function Home() {
   return (
     <div className="bg-black min-h-screen flex flex-col items-center justify-center space-y-5">
       <h1 className="text-white text-5xl">Calculator</h1>
-      <div className="bg-white px-10 py-5 rounded text-5xl my-10">{result}</div>
+      <div data-testid="result" className="bg-white px-10 py-5 rounded text-5xl my-10">{result}</div>
       <div className="flex flex-col items-center justify-center space-y-5 sm:space-y-0 sm:flex-row sm:space-x-5">
         <input
           className="py-2 rounded text-center text-xl"
           type="number"
           value={num1}
           onChange={(e) => setNum1(e.target.value)}
+          data-testid="num1"
         />
         <input
           className="py-2 rounded text-center text-xl"
           type="number"
           value={num2}
           onChange={(e) => setNum2(e.target.value)}
+          data-testid="num2"
         />
       </div>
       <div className="flex flex-col items-center justify-center space-y-5 sm:space-y-0 sm:flex-row sm:space-x-5">
-        <button className="bg-red-300 py-2 px-10 rounded" onClick={add}>
+        <button data-testid="add" className="bg-red-300 py-2 px-10 rounded" onClick={add}>
           Add
         </button>
-        <button className="bg-blue-300 py-2 px-10 rounded" onClick={subtract}>
+        <button data-testid="subtract" className="bg-blue-300 py-2 px-10 rounded" onClick={subtract}>
           Subtract
         </button>
-        <button className="bg-yellow-300 py-2 px-10 rounded" onClick={multiply}>
+        <button data-testid="multiply" className="bg-yellow-300 py-2 px-10 rounded" onClick={multiply}>
           Multiply
         </button>
-        <button className="bg-green-300 py-2 px-10 rounded" onClick={divide}>
+        <button data-testid="divide" className="bg-green-300 py-2 px-10 rounded" onClick={divide}>
           Divide
         </button>
       </div>
       <button
+      data-testid="reset"
         onClick={reset}
         className="bg-white text-black px-10 py-2 rounded font-semibold text-2xl mt-5"
       >
